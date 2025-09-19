07ed27d9064afadadc8ea36ccb5950c3aad0e3c9
flebedev77 <flebedev99@gmail.com>
2025-09-19 20:14:39 +0930

    Improvements to printing functionality
    Newline support
    Foreground and background color support
    Cursor structure

--- src/kernel.c
+++ src/kernel.c
@@ -33,13 +33,55 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 };
 
+
 uint16_t* vga_mem_ptr = (uint16_t*)VGA_MEMORY;
 
-// static inline uint16_t term_vga_char(
+typedef uint8_t vga_color_t;
 
-void term_print(const char* text) {
+struct term_cursor {
+  size_t x, y;
+};
+
+struct term_cursor cursor = {0};
+
+static inline vga_color_t vga_color(enum vga_color fg, enum vga_color bg) {
+  return (bg << 4) | fg;
+}
+
+static inline void term_cursor_advance(struct term_cursor* cursor) {
+  if (cursor == NULL)
+    return;
+
+  cursor->x++;
+  if (cursor->x > VGA_WIDTH) {
+    cursor->x = 0;
+    cursor->y++;
+
+    if (cursor->y > VGA_HEIGHT) {
+      // TODO: scroll terminal here
+      cursor->y--;
+    }
+  }
+}
+static inline void term_cursor_advanceln(struct term_cursor* cursor) {
+  if (cursor == NULL) return;
+  cursor->x = 0;
+  cursor->y++;
+}
+
+static inline size_t term_cursorpos_to_vga_idx(struct term_cursor* cursor) {
+  if (cursor == NULL) return 0;
+  return (cursor->y * VGA_WIDTH) + cursor->x;
+}
+
+void term_print(const char* text, vga_color_t color) {
   for (size_t i = 0; text[i] != 0; i++) {
-    vga_mem_ptr[i] = text[i] | (VGA_COLOR_RED << 8);
+    if (text[i] == '\n') {
+      term_cursor_advanceln(&cursor);
+      continue;
+    }
+    vga_mem_ptr[term_cursorpos_to_vga_idx(&cursor)] = text[i] | (color << 8);
+    term_cursor_advance(&cursor);    
   }
 }
 
@@ -53,7 +95,8 @@ void term_clear() {
 
 void k_main(void) {
   term_clear();
-  term_print("Hello world!");
+  term_print("Hello world!\nHi from a new line!\n\n", vga_color(VGA_COLOR_RED, VGA_COLOR_WHITE));
+  term_print("Hi from a new line and different color!\n", vga_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK));
 
   while (true) {}
 }
