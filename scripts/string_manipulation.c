2902620cfd6283372f8d60fe5231249dd4a209b9
flebedev77 <flebedev99@gmail.com>
2025-10-02 18:47:20 +0930

    Symbol to lower and to upper support

--- scripts/string_manipulation.c
+++ scripts/string_manipulation.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
+
+#define ALPHABET_LEN 25
+#define SPECIALCHR_LEN 7 // The amount of special characters in between the upper case alphabet and lowercase alphabet
+#define ASCII_NUMBERS_BEGIN 48
 
 void int_to_str(int num, char *str) {
     int i = 0;
@@ -65,14 +70,116 @@ void int32_to_hex(uint32_t num, char* str) {
   str[idx] = 0;
 }
 
-int main() {
-  char buf[100];
-  // int_to_str(-1234, buf);
-  // byte_to_hex(253, buf);
-  int val = 0;
-  for (int i = 0; i < 100; i++) {
-    val++;
-    int32_to_hex(val, buf);
-    printf("Mine: %s  Libc: %X\n", buf, val);
+bool is_symbol(char c) { // ASCII decided to sprinkle the symbols about
+  if (c > 'Z' && c < 'a') return true;
+  if (c > 'z') return true;
+  if (c < 'A' && c > ' ') return true;
+  return false;
+}
+
+char char_to_upper(char c) {
+  if (is_symbol(c)) return symbol_to_upper(c);
+  if (c > 'z' || c < 'a') return c; // already capital
+  return c - (ALPHABET_LEN + SPECIALCHR_LEN);
+}
+
+char char_to_lower(char c) {
+  if (is_symbol(c)) return symbol_to_lower(c);
+  if (c > 'Z' || c < 'A') return c; // already lowercase
+  return c + (ALPHABET_LEN + SPECIALCHR_LEN);
+}
+
+// Fuck you ascii
+char symbol_upper_table[255] = {0};
+char symbol_lower_table[255] = {0};
+
+char symbol_to_upper(char s) {
+  if (!is_symbol(s)) return s;
+  char out = symbol_upper_table[s];
+  if (!is_symbol(out)) return s;
+  return out;
+}
+char symbol_to_lower(char s) {
+  if (!is_symbol(s)) return s;
+  char out = symbol_lower_table[s];
+  if (!is_symbol(out)) return s;
+  return out;
+}
+
+void generate_symbol_lower_table() {
+  symbol_upper_table['1'] = '!';
+  symbol_upper_table['2'] = '@';
+  symbol_upper_table['3'] = '#';
+  symbol_upper_table['4'] = '$';
+  symbol_upper_table['5'] = '%';
+  symbol_upper_table['6'] = '^';
+  symbol_upper_table['7'] = '&';
+  symbol_upper_table['8'] = '*';
+  symbol_upper_table['9'] = '(';
+  symbol_upper_table['0'] = ')';
+
+  symbol_upper_table['-'] = '_';
+  symbol_upper_table['='] = '+';
+  symbol_upper_table['`'] = '~';
+  symbol_upper_table['['] = '{';
+  symbol_upper_table[']'] = '}';
+  symbol_upper_table['\\'] = '|';
+  symbol_upper_table[','] = '<';
+  symbol_upper_table['.'] = '>';
+  symbol_upper_table['/'] = '?';
+  symbol_upper_table[';'] = ':';
+  symbol_upper_table['\''] = '"';
+
+  for (uint8_t i = 0; i < 255; i++) {
+    symbol_lower_table[symbol_upper_table[i]] = i;
   }
+}
+
+
+int main() {
+  generate_symbol_lower_table();
+  if (1) {
+    printf("Characters\n");
+    char* test = "abcdefghijklmnopqrstuvwxyz";
+    size_t len = strlen(test);
+    char test2[len];
+
+    for (size_t i = 0; i < len; i++) {
+      test2[i] = char_to_upper(test[i]);
+      printf("%c:%c ", test[i], test2[i]);
+    }
+    printf("To upper\n");
+    for (size_t i = 0; i < len; i++) {
+      printf("%c:%c ", test2[i], char_to_lower(test2[i]));
+    }
+    printf("To lower\nSymbols\n");
+  }
+
+  if (1) {
+    char* test = "1234567890-=`[]\\,./;'";
+    size_t len = strlen(test);
+    char test2[len];
+
+    for (size_t i = 0; i < len; i++) {
+      test2[i] = symbol_to_upper(test[i]);
+      printf("%c:%c ", test[i], test2[i]);
+    }
+    printf("To upper\n");
+    for (size_t i = 0; i < len; i++) {
+      printf("%c:%c ", test2[i], symbol_to_lower(test2[i]));
+    }
+    printf("To lower\n");
+  }
+
+
+  
+  // char buf[100];
+  // // int_to_str(-1234, buf);
+  // // byte_to_hex(253, buf);
+  // int val = 0;
+  // for (int i = 0; i < 100; i++) {
+  //   val++;
+  //   int32_to_hex(val, buf);
+  //   printf("Mine: %s  Libc: %X\n", buf, val);
+  // }
 }
